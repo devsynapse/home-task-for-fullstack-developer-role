6b5ca2afb8336d329178bff4825f50d23be9dd31
Synapse Dev <synapsesoftdev@gmail.com>
2024-07-18 12:20:15 -0600

    products microservice created

---
+++ api.products/api/blueprints/products.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from api.models import Products
+from api.schemas import ProductSchema
+
+products_blueprint = Blueprint('products_blueprint', __name__)
+
+@products_blueprint.route('/', methods=['GET'])
+def get_active_products():
+    product_schema = ProductSchema(many=True)
+    try:
+        products = Products.select().where(
+            (Products.ProductStatus == 'Active')
+        ).dicts()
+
+        products_serialized = product_schema.dump(products)
+    except Exception as err:
+        return { 'data': [], 'message': str(err) }, 500
+    return { 'data': products_serialized, 'message': '' }, 200
+
+
